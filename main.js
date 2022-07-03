99eebf7650a813b2409f729f7a342ea9add0831b
Xearper <54599315+Xearper@users.noreply.github.com>
2022-07-03 19:07:10 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                     : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const menu = document.getElementById('menuBar');
 const mamacita = document.querySelector('.navbar-list-1');
 
@@ -40,5 +66,32 @@ nightmode.addEventListener("click", () => {
 
 })
 
-document.querySelector(".navbar-list-1").style.transition = "all .5s";
+var swiper = new Swiper(".mySwiper", {
 
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 5,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+    
+  });
+
+
+  
+  
