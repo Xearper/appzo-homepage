843a8433a8a739f9b28360da8cb9a390e5126131
Xearper <54599315+Xearper@users.noreply.github.com>
2022-07-04 19:31:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -33,26 +33,7 @@ const mamacita = document.querySelector('.navbar-list-1');
 
 
 
-// open modal
-    const kayitOl = document.querySelector(".kayit-btn"),
-    modal = document.querySelector(".modal");
-    kayitOl.addEventListener("click", () => {
-        modal.style.display = "flex";
-    });
 
- const mobilKayit = document.querySelector(".kayit-btn2");
- mobilKayit.addEventListener("click", () => {
-     modal.style.display = "flex";
- })
-
-
-  // close modal
-
-  const closeIcon = document.querySelector(".close-modal-icon");
-
-  closeIcon.addEventListener("click", () => {
-      modal.style.display = "none";
-  })
 
 //night mode
 const anan = document.querySelector (".ana-sayfa");
